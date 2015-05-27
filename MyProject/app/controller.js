0232ef937c8cea30a414686d685f164f21db55a8
ha-taka <haruna.takahashi@harmonicinc.com>
2015-05-27 14:38:30 -0700

    validation, use controllAs

--- MyProject/app/controller.js
+++ MyProject/app/controller.js
@@ -1,49 +1,79 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('ProductListCtrl', function($scope) {
-    $scope.products = [
-        {'Title': 'tomatoes',
-        'SKU': '123',
-        'Price': '$3.32'},
-        {'Title': 'potatoes',
-         'SKU': '345',
-         'Price': '$4.32'}
+myApp.controller('ProductListCtrl', function() {
+    this.products = [
+        {Title: 'tomatoes',
+        SKU: '123',
+        Price: '3.32'},
+        {Title: 'potatoes',
+         SKU: '345',
+         Price: '4.32'}
     ];
 
-    $scope.remove = function(index) {
+    this.remove = function(index) {
         if (confirm("Are you sure to delete?")) {
-            $scope.products.splice(index, 1);
+            this.products.splice(index, 1);
+        }
+
+        this.editing = false;
+    };
+
+    this.edit = function(index) {
+        this.Title = this.products[index].Title;
+        this.SKU = this.products[index].SKU;
+        this.Price = this.products[index].Price;
+
+        this.editing = true;
+        this.currentIndex = index;
+    };
+
+    this.update = function(index) {
+        if (this.validateFields(index) === true) {
+            this.products[index].Title = this.Title;
+            this.products[index].SKU = this.SKU;
+            this.products[index].Price = this.Price;
+
+            this.clearFields();
+            this.editing = false;
+        }
+
+    };
+
+    this.add = function() {
+        if (this.validateFields() === true) {
+            this.products.push({'Title': this.Title, 'SKU': this.SKU, 'Price': this.Price});
+            this.clearFields();
         }
     };
 
-    $scope.edit = function(index) {
-        $scope.Title = $scope.products[index].Title;
-        $scope.SKU = $scope.products[index].SKU;
-        $scope.Price = $scope.products[index].Price;
+    this.validateFields = function(index) {
+        for (var i = 0; i < this.products.length; i++) {
+            if (this.products[i].SKU === this.SKU) {
+                if (index !== i) {
+                    alert("SKU needs to be unique!");
+                    return false;
+                }
+            }
+        }
 
-        $scope.editing = true;
-        $scope.currentIndex = index;
+        if (isNaN(this.Price)) {
+            alert("Price needs to be number!");
+            return false;
+        }
+
+        return true;
     };
 
-    $scope.update = function(index) {
-// ToDo: check if SKU is unique, check if Price is a valid number
-        $scope.products[index].Title = $scope.Title;
-        $scope.products[index].SKU = $scope.SKU;
-        $scope.products[index].Price = $scope.Price;
+    this.clearFields = function() {
+        this.Title="";
+        this.SKU="";
+        this.Price="";
+    }
 
-        clearFields();
-        $scope.editing = false;
-    };
-
-    $scope.add = function() {
-// ToDo: check if SKU is unique, check if Price is a valid number
-        $scope.products.push({'Title':$scope.Title, 'SKU':$scope.SKU, 'Price':'$' + $scope.Price});
-        clearFields();
-    };
-    
-    function clearFields() {
-        $scope.Title="";
-        $scope.SKU="";
-        $scope.Price="";
+    this.export = function() {
+        console.log("***** Products List *****")
+        angular.forEach(this.products, function(product) {
+            console.log(product);
+        })
     }
 });
