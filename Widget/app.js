0b296d335670d0fee7cf649b455374d4310a54a0
ha-taka <haruna.takahashi@harmonicinc.com>
2015-06-04 12:32:02 -0700

    update for 6/4/15

--- Widget/app.js
+++ Widget/app.js
@@ -1,82 +1,13 @@
 /**
  * Created by haruna on 5/22/2015.
  */
-$(function() {
-    var selectedIndex = -1;
 
+$(function() {
     $('#products').listWidget();
 
-    $('#add').click(function(){
-        if (validateFields()) {
-            $('#products').listWidget('add', {
-                name: $('#productname').val(),
-                sku: $('#sku').val(),
-                price: $('#price').val() }
-            );
-            cleanFields();
-        }
-    });
-
-    $('#update').click(function(){
-        if (selectedIndex !== -1) {
-            if (validateFields(selectedIndex)) {
-                $('#products').listWidget('update', selectedIndex, {
-                    name: $('#productname').val(),
-                    sku: $('#sku').val(),
-                    price: $('#price').val()
-                });
-
-                cleanFields();
-                $(this).prop('disabled', true);
-                selectedIndex = -1;
-            }
-        }
-        else {
-            alert('You need to select "edit" first!');
-        }
-    });
-
-    $('#export').click(function() {
-        $('#products').listWidget('print');
-    });
-
-    // todo not getting triggered for some reason
-    $('#products').on( 'listWidgetedited', function( event, data, index ) {
-        $('#productname').val(data.name);
-        $('#sku').val(data.sku);
-        $('#price').val(data.price);
-        $(this).prop('disabled', false);
-        selectedIndex = index;
-    });
-
-    function cleanFields() {
-        $('#productname').val('');
-        $('#sku').val('');
-        $('#price').val('');
-    }
-
-    function validateFields(index) {
-        if ($('#productname').val() === '' || $('#sku').val() === '' || $('#price').val() === '') {
-            alert('all fields required!');
-       //     $('.errorMsg').val('All fields required')
-            return false;
-        }
-
-        if ($('#products').listWidget('exists', 'sku', $('#sku').val(), index)) {
-            alert('SKU needs to be unique!');
-            $('#sku').addClass('error');
-            return false;
-        }
-
-        if (isNaN($('#price').val())) {
-            alert('Price needs to be number!');
-            return false;
-        }
-
-        return true;
-    }
-
-    $('input:text').change(function() {
-        $(this).removeClass('error');
+    $("products").listWidget('setValidators',  {
+        'name': { required: true },
+        'sku': { required: true, unique: true },
+        'price': { required: true, number: true }
     })
 });
--- Widget/listWidget.js
+++ Widget/listWidget.js
@@ -3,73 +3,132 @@
  */
 
 (function($) {
+    var VALIDATORS = {
+        required: function(value) {
+            return !value;
+        },
+        unique: function(value, items, fieldName) {
+            for (var i = 0; i < items.length; i++) {
+                if (items[i][fieldName] === value) {
+                    return false;
+                }
+            }
+
+            return true;
+        },
+        number: function(value) {
+            return !isNaN(value);
+        }
+    };
+
     $.widget('ui.listWidget', {
-        _init: function() {
+        _init: function () {
         },
 
-    _create: function() {
-          this._on({
-              'click .delete': 'removeItem',
-              'click .edit': 'editItem'
-          });
-          items = [];
+        _create: function () {
+            this._table = this.element.find('[data-part="grid"]');
+            this._form = this.element.find('[data-part="form"]');
+            this._rowTemplate = this._table.find('[data-part="grid-row"]').remove();
+            this._items = [];
+            this._on({
+                'click [name=delete]': 'removeItem',
+                'click [name=edit]': 'editItem',
+                'click [name=add]': 'addItem',
+                'click [name=update]': 'updateItem',
+                'click [name=export]': 'print'
+            });
+            selectedIndex = -1;
+        },
+
+        setValidators: function(validators) {
+            for (var fieldName in validators) {
+                var fieldValidators = validators[fieldName];
+                this._on(this._form[fieldName], { change: this._handleChangeEvent(fieldName, fieldValidators) });
+            }
+        },
+
+        _handleChangeEvent: function(fieldName, validators) {
+            return function(event) {
+                var error_element = event.target.parent().span;
+                $.each(validators, function(validatorName) {
+                    VALIDATORS[validatorName](value, this._items, fieldName) ? error_element.removeClass("error").addClass("no_error"):error_element.removeClass("no_error").addClass("error");
+                })
+            };
+        },
+
+        _rowFor: function(item) {
+            var itemNode = this._rowTemplate.clone();
+
+            itemNode.each(function(i, cellNode) {
+                var cell = $(cellNode);
+                var text = cell.html().replace(/\{\{\s*(\w+)\s*\}\}/g, function(match, capture) {
+                    return item[capture];
+                });
+
+                cell.html(text);
+            });
+
+            return itemNode;
         },
 
         removeItem: function(event) {
             if (confirm('are you sure to delete?')) {
                 var item = $(event.target).closest('tr');
+                this._items.splice(item.index(), 1);
                 item.remove();
-                items.splice(item.index(), 1);
             }
         },
 
         editItem: function(event) {
             var index = $(event.target).closest('tr').index();
-            this._trigger('edited', event, [items[index], index]);
+            this.objToForm(this._items[index]);
+            selectedIndex = index;
+            this._form.update.disabled = true;
         },
 
-        add: function(object) {
-            var line = '<tr>';
-            $.each(object, function(key, value){
-                line += '<td>' + value + '</td>';
-            });
+        addItem: function(event) {
+            event.preventDefault();
 
-            line += '<td><button class="edit">edit</button></td>';
-            line += '<td><button class="delete">delete</button></td>';
-            line += '</tr>';
+            var obj = this.objFromForm();
 
-            this.element.append(line);
-            items.push(object);
+            this._table.append(this._rowFor(obj));
+            this._items.push(obj);
         },
 
-        update: function(index, object) {
-            var line = '<tr>';
-            $.each(object, function(key, value){
-                line += '<td>' + value + '</td>';
-            });
+        updateItem: function(event) {
+            event.preventDefault();
 
-            line += '<td><button class="edit">edit</button></td>';
-            line += '<td><button class="delete">delete</button></td>';
-            line += '</tr>';
+            var obj = this.objFromForm();
 
-            $('tbody tr:eq(' + index + ')').replaceWith(line);
-            items[index] = object;
+            $("tbody tr:eq(" + selectedIndex + ")").replaceWith(this._rowFor(obj));
+//            this._table['tbody tr:eq(' + selectedIndex + ')'].replaceWith(this._rowFor(obj));
+            this._items[selectedIndex] = obj;
         },
-// todo        global search. You need to find elements relative to your this.element
 
         print: function() {
             console.log('***** Products List *****');
-            console.log(JSON.stringify(items));
+            console.log(JSON.stringify(this._items));
         },
 
-        exists: function(key, value, index) {
-            for (var i = 0; i < items.length; i++) {
-                if (index !== i && items[i][key] === value) {
-                    return true;
-                }
+        objFromForm: function() {
+            var form = this.element.find('[data-part="form"]').get(0);
+            var product = 'name sku price'.split(' ').reduce(function(product, fieldName) {
+                var field = form[fieldName];
+                product[field.name] = field.value;
+
+                return product;
+            }, {});
+
+            return product;
+        },
+
+        objToForm: function(object) {
+            var form = this.element.find('[data-part="form"]').get(0);
+            for (var prop in object) {
+                var field = form[prop];
+                field.value = object[prop];
             }
 
-            return false;
         }
     });
 }(jQuery));
